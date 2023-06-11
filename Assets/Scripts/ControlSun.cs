43db39474ea2be9299f94985dc10deb0db15ac60
Phan Minh Trường <104303855+MinhTruongDev@users.noreply.github.com>
2023-06-11 12:45:33 +0700

    Change Sun Angle base on Time

--- Assets/Scripts/ControlSun.cs
+++ Assets/Scripts/ControlSun.cs
@@ -2,33 +2,72 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlSun : MonoBehaviour
 {
-    [SerializeField] private bool isChanging = false;
-    [SerializeField] private float latitude;
-    [SerializeField] private float longitude;
-    [SerializeField] private DateTime currentDate;
-    [SerializeField] private DateTime Time;
+    [SerializeField] InputField latitudeInput;
+    [SerializeField] InputField longitudeInput;
+    [SerializeField] InputField currentDateInput;
+    [SerializeField] InputField timeIpnut;
 
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject sun;
+    [SerializeField] Light sunLight;
+
+    [SerializeField] [Range(-90f, 90f)] private float latitude;
+    [SerializeField] [Range(-180f, 180f)] private float longitude;
+    [SerializeField] [Range(1, 31)] private int currentDate;
+    [SerializeField] [Range(1, 12)] private int currentMonth;
+    [SerializeField] [Range(1900, 2030)] private int currentYear;
+    [SerializeField] [Range(1, 60)] private int second;
+    [SerializeField] [Range(1, 60)] private int minute;
+    [SerializeField] [Range(0, 24)] private int hour;
+
+    [Range(0, 43200)] private float time;
+
+
+
+    private void Start()
     {
-        
+        sun = gameObject;
+        sunLight = gameObject.GetComponent<Light>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        if(isChanging)
-        {
-            MoveSun();
-        }
+        MoveSun(latitude, longitude, currentDate, currentMonth, currentYear, hour, minute, second);
+
+    }   
+
+    private void MoveSun(float latitude, float longitude, int date, int month, int year, int hour, int minute, int second)
+    {
+        int tempMonth = FindSunHeight(month);
+        time = (hour * 360) + (minute * 60) + second;
+        sun.transform.localEulerAngles = new Vector3(Time.deltaTime * time, tempMonth * 40 * 0.2f, 0);
     }
 
-    private void MoveSun()
+   
+
+    private static int FindSunHeight(int month)
     {
-        throw new NotImplementedException();
+
+        if (month <= 3)
+        {
+            return 3;
+        }
+        else if (month <= 6 && month > 3)
+        {
+            return 6;
+        }
+        else if (month <= 9 && month > 6)
+        {
+            return 9;
+        }
+        else
+        {
+            return 12;
+        }
+
     }
 }
